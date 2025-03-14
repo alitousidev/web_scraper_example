6cdb2edc235fc00c339ee7d3604b574d3f6ea01d
Ali Tousi <toosi.ali@gmail.com>
2025-03-13 21:43:37 -0700

    Update project page with demo GIFs

--- json_scraper.py
+++ json_scraper.py
@@ -12,6 +12,14 @@ try:
     response.raise_for_status()  # چک کردن خطاهای HTTP (مثل 404، 500 و...)
     print("Request was successful!")
     print("Response Data:", response.json())
+    <h3>⿡ Running the Scraper</h3>
+    <p>This GIF shows how the scraper is executed in the terminal.</p>
+    <img src="../assets/json_scraper.gif" alt="Running the Web Scraper" width="600">
+    <h3>⿡ Running the Scraper</h3>
+    <p>This GIF shows how the scraper is executed in the terminal.</p>
+    <img src="../assets/quotes_scraper.gif" alt="Running the Web Scraper" width="600">
+
+
 except Timeout:
     print(f"The request timed out after {timeout} seconds.")
 except RequestException as e:
